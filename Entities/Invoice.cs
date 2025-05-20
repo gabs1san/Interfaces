85e9c2f5a4d70217c50787dedde9773700cecbb6
gabs1san <sanbiel398@gmail.com>
2025-05-20 18:20:42 -0300

    Adicionando as classes de serviço e de entidades

---
+++ Entities/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+
+namespace Interfaces.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayent { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayent, double tax)
+        {
+            BasicPayent = basicPayent;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayent + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayent.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal Payment: "
+                + TotalPayment.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Interfaces.Entities;
+using System;
+using System.Globalization;
 
 namespace Interfaces
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            
+        {
+            Console.WriteLine("ENter rental data: ");
+            Console.WriteLine("Car model:");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
         }
     }
